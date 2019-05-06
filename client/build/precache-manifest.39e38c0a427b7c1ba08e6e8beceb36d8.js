08eb2d23903916403876fcd052b47098150db68e
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-05-05 21:21:30 -0700

    run build

---
+++ client/build/precache-manifest.39e38c0a427b7c1ba08e6e8beceb36d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac8e5ccfffd4dbcb6b121ca94fadaa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c29d46fb77879993b443",
+    "url": "/static/css/main.abc9a915.chunk.css"
+  },
+  {
+    "revision": "e23f46e4b475379f4fd1",
+    "url": "/static/js/2.1f6f2aa3.chunk.js"
+  },
+  {
+    "revision": "c29d46fb77879993b443",
+    "url": "/static/js/main.27ca5fa2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "47b8d4ee8b0f9189bbd34799acae9738",
+    "url": "/static/media/book.47b8d4ee.jpeg"
+  }
+]);
