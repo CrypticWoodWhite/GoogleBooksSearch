7aa69d20aca1ad11b9aab164109de797f4a8db56
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-05-07 21:45:25 -0700

    path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const router = require("./routes/apiRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
