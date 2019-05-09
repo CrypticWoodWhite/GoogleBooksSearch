1a96e23629d67f00f50c6015fd86a704e53457e3
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-05-09 10:32:29 -0700

    rm compdidmt

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -26,7 +26,7 @@ class Search extends Component {
     };
 
     componentDidMount() {
-        this.getSavedBooks()
+        // this.getSavedBooks()
         console.log("state isarray books: " + Array.isArray(this.state.books));
         console.log("state isarray savedbooks: " + Array.isArray(this.state.savedBooks));
     };
@@ -34,7 +34,6 @@ class Search extends Component {
     getSavedBooks = () => {
         API.getSavedBooks()
             .then(res => {
-                console.log("res: " + JSON.stringify(res));
                 console.log("res.data: " + res.data);
                 this.setState({
                     savedBooks: res.data
