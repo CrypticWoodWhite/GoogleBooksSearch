afffa7844516dd66490b0a9b85f414f72ade3b8a
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-05-09 15:57:05 -0700

    server'

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("build"));
 }
 
 app.use("/api/books", router);
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
